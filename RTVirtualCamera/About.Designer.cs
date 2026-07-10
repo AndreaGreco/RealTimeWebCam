bb9847a66d29db290d68b70e6b535ff21c0d0bac
Andrea Greco <a.greco@4sigma.it>
2026-07-10 11:09:59 +0200

    Rework over About page

--- RTVirtualCamera/About.Designer.cs
+++ RTVirtualCamera/About.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace RTVirtualCamera
+namespace RTVirtualCamera
 {
     partial class About
     {
@@ -28,53 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
-            label1 = new System.Windows.Forms.Label();
-            version_lbl = new System.Windows.Forms.Label();
+            appIconBox = new System.Windows.Forms.PictureBox();
+            titleLabel = new System.Windows.Forms.Label();
+            versionLabel = new System.Windows.Forms.Label();
+            taglineLabel = new System.Windows.Forms.Label();
+            separatorLabel = new System.Windows.Forms.Label();
+            creditsLabel = new System.Windows.Forms.Label();
+            githubLink = new System.Windows.Forms.LinkLabel();
+            closeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)appIconBox).BeginInit();
             SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(15, 15);
-            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(48, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Version:";
-            // 
-            // version_lbl
-            // 
-            version_lbl.AutoSize = true;
-            version_lbl.Location = new System.Drawing.Point(75, 15);
-            version_lbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            version_lbl.Name = "version_lbl";
-            version_lbl.Size = new System.Drawing.Size(38, 15);
-            version_lbl.TabIndex = 1;
-            version_lbl.Text = "label2";
-            // 
+            //
+            // appIconBox
+            //
+            appIconBox.Location = new System.Drawing.Point(20, 20);
+            appIconBox.Name = "appIconBox";
+            appIconBox.Size = new System.Drawing.Size(48, 48);
+            appIconBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            appIconBox.TabIndex = 0;
+            appIconBox.TabStop = false;
+            //
+            // titleLabel
+            //
+            titleLabel.AutoSize = true;
+            titleLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            titleLabel.Location = new System.Drawing.Point(80, 18);
+            titleLabel.Name = "titleLabel";
+            titleLabel.Size = new System.Drawing.Size(150, 21);
+            titleLabel.TabIndex = 1;
+            titleLabel.Text = "RT Virtual Camera";
+            //
+            // versionLabel
+            //
+            versionLabel.AutoSize = true;
+            versionLabel.ForeColor = System.Drawing.SystemColors.GrayText;
+            versionLabel.Location = new System.Drawing.Point(82, 44);
+            versionLabel.Name = "versionLabel";
+            versionLabel.Size = new System.Drawing.Size(60, 15);
+            versionLabel.TabIndex = 2;
+            versionLabel.Text = "Version";
+            //
+            // taglineLabel
+            //
+            taglineLabel.Location = new System.Drawing.Point(20, 80);
+            taglineLabel.Name = "taglineLabel";
+            taglineLabel.Size = new System.Drawing.Size(400, 32);
+            taglineLabel.TabIndex = 3;
+            taglineLabel.Text = "Tagline";
+            //
+            // separatorLabel
+            //
+            separatorLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            separatorLabel.Location = new System.Drawing.Point(20, 114);
+            separatorLabel.Name = "separatorLabel";
+            separatorLabel.Size = new System.Drawing.Size(400, 2);
+            separatorLabel.TabIndex = 4;
+            //
+            // creditsLabel
+            //
+            creditsLabel.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            creditsLabel.ForeColor = System.Drawing.SystemColors.GrayText;
+            creditsLabel.Location = new System.Drawing.Point(20, 124);
+            creditsLabel.Name = "creditsLabel";
+            creditsLabel.Size = new System.Drawing.Size(400, 34);
+            creditsLabel.TabIndex = 5;
+            creditsLabel.Text = "Credits";
+            //
+            // githubLink
+            //
+            githubLink.AutoSize = true;
+            githubLink.LinkColor = System.Drawing.SystemColors.HotTrack;
+            githubLink.Location = new System.Drawing.Point(20, 162);
+            githubLink.Name = "githubLink";
+            githubLink.Size = new System.Drawing.Size(220, 15);
+            githubLink.TabIndex = 6;
+            githubLink.TabStop = true;
+            githubLink.Text = "github.com/AndreaGreco/RealTimeWebCam";
+            githubLink.LinkClicked += GithubLink_LinkClicked;
+            //
+            // closeButton
+            //
+            closeButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            closeButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            closeButton.Location = new System.Drawing.Point(344, 190);
+            closeButton.Name = "closeButton";
+            closeButton.Size = new System.Drawing.Size(76, 26);
+            closeButton.TabIndex = 7;
+            closeButton.Text = "Close";
+            closeButton.UseVisualStyleBackColor = true;
+            //
             // About
-            // 
+            //
+            AcceptButton = closeButton;
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(415, 270);
-            Controls.Add(version_lbl);
-            Controls.Add(label1);
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
-            Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            CancelButton = closeButton;
+            ClientSize = new System.Drawing.Size(440, 236);
+            Controls.Add(closeButton);
+            Controls.Add(githubLink);
+            Controls.Add(creditsLabel);
+            Controls.Add(separatorLabel);
+            Controls.Add(taglineLabel);
+            Controls.Add(versionLabel);
+            Controls.Add(titleLabel);
+            Controls.Add(appIconBox);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "About";
-            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             Text = "About";
             Load += About_Load;
+            ((System.ComponentModel.ISupportInitialize)appIconBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
-
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label version_lbl;
+        private System.Windows.Forms.PictureBox appIconBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Label taglineLabel;
+        private System.Windows.Forms.Label separatorLabel;
+        private System.Windows.Forms.Label creditsLabel;
+        private System.Windows.Forms.LinkLabel githubLink;
+        private System.Windows.Forms.Button closeButton;
     }
 }
--- RTVirtualCamera/About.cs
+++ RTVirtualCamera/About.cs
@@ -1,10 +1,14 @@
-﻿using System;
+using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace RTVirtualCamera
 {
     public partial class About : Form
     {
+        private const string ProjectUrl = "https://github.com/AndreaGreco/RealTimeWebCam";
+
         public About()
         {
             InitializeComponent();
@@ -12,7 +16,43 @@ namespace RTVirtualCamera
 
         private void About_Load(object sender, EventArgs e)
         {
-            this.version_lbl.Text = Application.ProductVersion;
+            Text = AppStrings.Get("App_Title");
+            titleLabel.Text = AppStrings.Get("App_Title");
+            versionLabel.Text = string.Format(AppStrings.Get("About_VersionFormat"), Application.ProductVersion);
+            taglineLabel.Text = AppStrings.Get("About_Tagline");
+            creditsLabel.Text = AppStrings.Get("About_Credits");
+            closeButton.Text = AppStrings.Get("Button_Close");
+
+            // Pull the icon straight out of the running exe (the one embedded via
+            // ApplicationIcon/Resources/icon.ico) instead of shipping a second,
+            // separately-maintained copy of the image as a form resource.
+            try
+            {
+                using (Icon exeIcon = Icon.ExtractAssociatedIcon(Application.ExecutablePath))
+                {
+                    if (exeIcon != null)
+                    {
+                        appIconBox.Image = exeIcon.ToBitmap();
+                    }
+                }
+            }
+            catch
+            {
+                // Non-critical: the dialog still works without the icon preview.
+            }
+        }
+
+        private void GithubLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(ProjectUrl) { UseShellExecute = true });
+            }
+            catch
+            {
+                // Non-critical: if the shell can't resolve a browser, there's nothing
+                // more useful to do than leave the label showing the URL as text.
+            }
         }
     }
 }
